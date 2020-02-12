d3b306804168f78c6386d64b20e58c932a4bf840
Joseph Liu <josephliu@Josephs-MacBook-Pro.local>
2020-02-12 11:53:58 -0500

    Update dotfiles

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-cp .bash_profile dotfiles/.bash_profile
-cp .git-prompt.sh dotfiles/.git-prompt.sh
-cp .vimrc dotfiles/.vimrc
-cp .tmux.conf dotfiles/.tmux.conf
-cp dotfiles.sh dotfiles/dotfiles.sh
-cd dotfiles
+cp ../.bash_profile dotfiles/.bash_profile
+cp ../.git-prompt.sh dotfiles/.git-prompt.sh
+cp ../.vimrc dotfiles/.vimrc
+cp ../.tmux.conf dotfiles/.tmux.conf
 git add .
 git commit -m "Update dotfiles"
 git push
